50cf416da74213af25fceb5ec34de49a6fd4efd5
Diego Romero <arias.dromero@gmail.com>
2023-10-26 18:56:57 -0600

    server setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+const app = express();
+
+app.post('/login', (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '4ad766fd20ac429ab9097075881f9f12',
+        clientSecret: 'd9bf5c2d81ee468cbc5c81dc63c7539a',
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    })
+});
